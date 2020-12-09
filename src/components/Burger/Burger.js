f2142b359b20f7ba4cbb3668aef6f0dc9a3bc1cc
Michelle Racho <rachoma12@gmail.com>
2020-12-09 17:45:42 -0500

    dynamically output BurgerIngredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,11 +5,22 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import styles from './Burger.module.css';
 
 const Burger = props => {
+  // convert ingredients state (object) to an array = ['salad', 'bacon', 'cheese','meat']
+  const transformedIngredients = Object.keys(props.ingredients).map(
+    ingredientKey => {
+      // groups each ingredient type to its own array
+      return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
+        return (
+          <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />
+        );
+      });
+    }
+  );
+
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
