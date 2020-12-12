d07e975d6a988cea05014fcced30084800ff0365
Michelle Racho <rachoma12@gmail.com>
2020-12-12 00:12:45 -0500

    improve performance by optimizing when the Modal updates

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,51 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
 import styles from './OrderSummary.module.css';
 
-const OrderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(
-    ingredientKey => {
-      return (
-        <li key={ingredientKey}>
-          <span
-            style={{
-              textTransform: 'capitalize',
-              marginRight: '.25rem'
-            }}
-          >
-            {ingredientKey}:
-          </span>
-          {props.ingredients[ingredientKey]}
-        </li>
-      );
-    }
-  );
+class OrderSummary extends Component {
+  // check when this component is rendered
+  componentDidUpdate() {
+    // componentWillUpdate() {
+    console.log('[OrderSummary] will update');
+  }
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul className={styles.OrderSummaryUl}>{ingredientSummary}</ul>
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      ingredientKey => {
+        return (
+          <li key={ingredientKey}>
+            <span
+              style={{
+                textTransform: 'capitalize',
+                marginRight: '.25rem'
+              }}
+            >
+              {ingredientKey}:
+            </span>
+            {this.props.ingredients[ingredientKey]}
+          </li>
+        );
+      }
+    );
 
-      <p>
-        <strong>Total Price: ${props.price.toFixed(2)}</strong>
-      </p>
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul className={styles.OrderSummaryUl}>{ingredientSummary}</ul>
 
-      <div className={styles.OrderSummaryContinue}>
-        <p>Continue to Checkout?</p>
-        <div>
-          <Button btnType="Danger" click={props.cancel}>
-            CANCEL
-          </Button>
-          <Button btnType="Success" click={props.continue}>
-            CONTINUE
-          </Button>
+        <p>
+          <strong>Total Price: ${this.props.price.toFixed(2)}</strong>
+        </p>
+
+        <div className={styles.OrderSummaryContinue}>
+          <p>Continue to Checkout?</p>
+          <div>
+            <Button btnType="Danger" click={this.props.cancel}>
+              CANCEL
+            </Button>
+            <Button btnType="Success" click={this.props.continue}>
+              CONTINUE
+            </Button>
+          </div>
         </div>
-      </div>
-    </Aux>
-  );
-};
+      </Aux>
+    );
+  }
+}
 
 export default OrderSummary;
